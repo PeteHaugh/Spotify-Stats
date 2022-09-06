7e2c485725164e5f0087155b4c43f02b83a43a6f
Peter Haugh <peter.ahaugh@gmail.com>
2022-09-06 21:56:52 +0100

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState, useLayoutEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation
+} from "react-router-dom";
+import { accessToken, logout, getCurrentUserProfile } from './Spotify'
+import { catchErrors } from './utils'
+
+const ScrollToTop = () => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return null;
+} 
+
 
 function App() {
+
+  const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null)
+
+  useEffect(() => {
+    setToken(accessToken);
+
+    const fetchData = async () => {
+      const {data} = await getCurrentUserProfile();
+      setProfile(data);
+    }
+
+    catchErrors(fetchData());
+
+  }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {!token ? (
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="http://localhost:8888/login"
         >
-          Learn React
+          Log in to Spotify
         </a>
+        ) : (
+          <Router>
+            <ScrollToTop />
+            <Routes>
+              <Route path="/top-artists" element={<h1>Top Artists</h1>}>
+              </Route>
+              <Route path="/top-tracks" element={<h1>Top Tracks</h1>}>
+                
+              </Route>
+              <Route path="/playlists/:id" element={<h1>Playlist</h1>}>
+                
+              </Route>
+              <Route path="/playlists" element={<h1>Playlists</h1>}>
+                
+              </Route>
+              <Route path="/" element={
+                <>
+                  <button onClick={logout}>Log Out</button>
+
+                  {profile && (
+                    <div>
+                      <h1>{profile.display_name}</h1>
+                      <p>{profile.followers.total} Followers</p>
+                      {profile.images.length && profile.images[0].url && (
+                        <img src={profile.images[0].url} alt="Avatar"/>
+                      )}
+                    </div>
+                  )}
+                </>
+              }>
+              </Route>
+            </Routes>
+          </Router>
+        )}
       </header>
     </div>
   );
